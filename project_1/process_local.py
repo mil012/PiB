ccbee1e7e2a108007003a1a6502cafa99ffbafe0
Simon Fong <simon@Simons-MacBook-Pro.local>
2018-05-02 11:17:20 -0700

    Fixed type 10x10 to 100x100 and added comments

--- project_1/process_local.py
+++ project_1/process_local.py
@@ -1,15 +1,15 @@
 """
 ECE196 Face Recognition Project
-Author: W Chen
+Author: Will Chen
 
 Prerequisite: You need to install OpenCV before running this code
 The code here is an example of what you can write to print out 'Hello World!'
 Now modify this code to process a local image and do the following:
-1. read geisel.jpg
-2. convert color to gray scale
-3. resize to half of its original dimensions
-4. draw a box at the center the image with size 10x10
-5. save image with a new name to local directory
+1. Read geisel.jpg
+2. Convert color to gray scale
+3. Resize to half of its original dimensions
+4. Draw a box at the center the image with size 100x100
+5. Save image with a new name to local directory
 All the above steps should be in one function called process_image()
 """
 
@@ -18,16 +18,16 @@ All the above steps should be in one function called process_image()
 def process_image():
     return
 
-
+# Just prints 'Hello World! to screen.'
 def hello_world():
     print 'Hello World!'
     return
 
-
+# TODO: Call process_image function.
 def main():
     hello_world()
     return
 
 
-if __name__ == '__main__':
+if(__name__ == '__main__'):
     main()
