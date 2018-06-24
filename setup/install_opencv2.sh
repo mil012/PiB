b90038f4e9f08508ae49795c5a4e04086e28613b
Simon <simonfong6@gmail.com>
2018-06-24 04:16:36 -0700

    Added comments for each library

--- setup/install_opencv2.sh
+++ setup/install_opencv2.sh
@@ -1,7 +1,33 @@
+#!/usr/bin/env bash
+
+# Script to install OpenCV on Raspberry Pi 3
+# Information from Will Chen and this website:
+# https://www.pyimagesearch.com/2017/09/04/raspbian-stretch-install-opencv-3-python-on-your-raspberry-pi/
+# Author: Simon Fong
+
+# Install everything in home directory.
 cd ~
-sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev
-libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev
-libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+# Developer tools used in build process
+sudo apt-get install build-essential cmake pkg-config git -y
+
+# Image I/O libraries
+sudo apt-get install libjpeg-dev libtiff-dev libjasper-dev libpng-dev -y
+
+# Video I/O libraries
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev -y
+
+# GUI libraries
+sudo apt-get install libgtk2.0-dev -y
+
+# Threaded, not sure
+sudo apt-get install libtbb2 libtbb-dev -y
+   
+# Cameras I/O
+sudo apt-get install libdc1394-22-dev -y
+   
+# Python and Numpy
+sudo apt-get install python-dev python-numpy -y
 
 #Get files
 wget -O opencv.zip https://github.com/opencv/opencv/archive/2.4.13.4.zip
