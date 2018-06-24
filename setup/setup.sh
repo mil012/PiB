fdc1d9e19d76fba7c6ebd1a68e7a446bace51300
Simon <simonfong6@gmail.com>
2018-06-24 02:17:12 -0700

    Added some basic setup scripts

---
+++ setup/setup.sh
@@ -0,0 +1,15 @@
+# Install various tools and perform environment setup for Raspberry Pi 3 for
+# face recognition project by Project in a Box.
+# Author: Simon Fong
+
+# General system updates
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install Vim for text editing
+sudo apt-get install vim -y
+
+# Set keyboard to US layout
+cp keyboard /etc/default/keyboard
+
+
