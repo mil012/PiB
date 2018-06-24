b297a15a58b6b6c0063527791f85f478ccec92cd
Simon <simonfong6@gmail.com>
2018-06-24 11:17:15 -0700

    Added Picamera setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,6 +4,9 @@
 # face recognition project by Project in a Box.
 # Author: Simon Fong
 
+# Notes:
+# PiCamera: https://raspberrypi.stackexchange.com/questions/14229/how-can-i-enable-the-camera-without-using-raspi-config
+
 # General system updates
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -17,5 +20,14 @@ sudo apt-get install vim -y
 
 # Set keyboard to US layout
 sudo cp keyboard /etc/default/keyboard
+
+# Enable PiCamera (Same from raspi-config)
+set_config_var start_x 1 /boot/config.txt
+CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
+if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+  set_config_var gpu_mem 128 /boot/config.txt
+fi
+sed /boot/config.txt -i -e "s/^startx/#startx/"
+sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
 
 
