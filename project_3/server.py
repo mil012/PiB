2d8915441cde4eb0b86e08c03e81130156501fbf
Simon <simonfong6@gmail.com>
2018-06-25 22:15:41 -0700

    Comments and index serving

--- project_3/server.py
+++ project_3/server.py
@@ -1,4 +1,6 @@
 """
+server.py
+
 ECE196 Face Recognition Project
 Author: Will Chen, Simon Fong
 
@@ -9,6 +11,9 @@ What this script should do:
     3.1 Save the image from the request.
     3.2 Load the image and classify it.
     3.3 Send the label and confidence back to requester(Pi).
+
+Installation:
+    pip install numpy keras tensorflow h5py flask flask-cors
 """
 import cv2
 import numpy as np
@@ -44,11 +49,12 @@ def classify(path_to_image):
     mean_pixel = np.array([104., 117., 123.]).reshape((1, 1, 3))
 
     # TODO: Use opencv to read and resize image to standard dimensions
-
+    img = #______
+    resized_img = #______
     
     # TODO: Subtract mean_pixel from the image store the new image in 
     # a variable called 'normalized_image'
-     
+    normalized_image = #________
     
     # Turns image shape of (2,) to (1,2)
     image_to_be_classified = np.expand_dims(normalized_image, axis=0)
@@ -59,23 +65,36 @@ def classify(path_to_image):
     # TODO: Use network to predict the 'image_to_be_classified' and
     # get an array of prediction values
     # Note: model.predict() returns an array of arrays ie. [[classes]]
-    
+    predictions = #______
     
     # TODO: Get the predicted label which is defined as follows:
     # Label = the index of the largest value in the prediction array
     # This label is a number, which corresponds to the same number you 
     # give to the folder when you organized data
     # Hint: np.argmax
+    label = #________
     
     
     # TODO: Calculate confidence according to the following metric:
     # Confidence = prediction_value / sum(all_prediction_values)
     # Be sure to call your confidence value 'conf'
+    # Hint: np.sum()
+    label_value = #_______
+    total = #_________
+    conf = #__________
+
     
     prediction = {'label': label,
                   'confidence': float(conf)}
 
-    return prediction
+    return 
+
+@app.route('/')
+def index():
+    """
+    Handles sending the webcam tool.
+    """
+    return send_from_directory('.','index.html')
 
 @app.route('/predict')
 def predict():
