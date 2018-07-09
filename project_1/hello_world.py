a95d55da8b3ec3d89d951efd39910331bf8ffeff
Simon <simonfong6@gmail.com>
2018-07-09 09:20:21 -0700

    Empty hello world

---
+++ project_1/hello_world.py
@@ -0,0 +1,12 @@
+"""
+hello_world.py
+
+Simple Hello World program.
+
+ECE196 Face Recognition Project
+Author: Will Chen
+
+1. Write a Write a program that prints "Hello World!" and uses the main function convention.
+"""
+
+# TODO: Write a program that prints "Hello World!" and uses a main function.
